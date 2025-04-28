de6cef22a43efd8fda1d932dce6aacf6b0296132
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-04-29 01:47:58 +0200

    Update build.gradle.kts

--- extensions/shared/build.gradle.kts
+++ extensions/shared/build.gradle.kts
@@ -1,3 +1,30 @@
+import java.lang.Boolean.TRUE
+
+android {
+    namespace = "app.revanced.extension"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = TRUE
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
 dependencies {
+    compileOnly(libs.annotation)
+    compileOnly(libs.preference)
+    implementation(libs.collections4)
+    implementation(libs.lang3)
     implementation(project(":extensions:shared:library"))
+
+    compileOnly(project(":extensions:shared:library"))
 }
